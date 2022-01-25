f33824cce2e4fc2c8f0c2b109368c28269b2d09c
MaxPaws <45854378+MaxPaws@users.noreply.github.com>
2022-01-25 23:42:32 +0300

    Initial commit

---
+++ SetUp_for_tests.py
@@ -0,0 +1,51 @@
+# Файл с настройками логгера и записью ответов на необходимые запросы на сайт в переменные,
+# для дальнейшего их использования в тестах.
+#
+# Логи запросов к эндпоинтам и их ответов записываются в файл "Test_Requests_To_Httpbin_Log.log"
+# после каждого запуска тестов.
+
+import logging
+import requests
+
+# Логгер, подхватывающий GET запросы библиотеки requests.
+# Запросы с их статусами пишутся и в консоль и в файл "Test_Requests_To_Httpbin_Log.log".
+log = logging.getLogger('urllib3')
+log.setLevel(logging.DEBUG)
+
+# Для обработчика - один для вывода в консоль, второй для записи в файл,
+# оба пишут события уровня DEBUG и выше.
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+tfh = logging.FileHandler("Test_Requests_To_Httpbin_Log.log", mode='w')
+tfh.setLevel(logging.DEBUG)
+
+# Форматтер для вывода записей в следующем стиле:
+# DEBUG - 25-Jan-22 21:12:33 - urllib3.connectionpool - сообщение
+lf = logging.Formatter('%(levelname)s - %(asctime)s - %(name)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+# Установка форматтера для обработчиков и обработчиков в логгер.
+ch.setFormatter(lf)
+tfh.setFormatter(lf)
+log.addHandler(ch)
+log.addHandler(tfh)
+
+# Далее идут сами запросы на сайт и сохранение ответов в переменные.
+# Воизбежание блокировки, между запросами можно было бы вызывать таймаут time.sleep(3).
+
+# GET запрос к "https://httpbin.org/headers" и логирование полученного ответа
+headers_page_response = requests.get("https://httpbin.org/headers")
+log.info(headers_page_response)
+
+# Сохранение тела ответа "https://httpbin.org/headers" в формате JSON и вывод тела в лог
+headers_page_response_body = headers_page_response.json()
+log.info(headers_page_response_body)
+
+# GET запрос к страницам "https://httpbin.org/status/статус_код" и логирование полученных ответов
+code_200_page_response = requests.get("https://httpbin.org/status/200")
+log.info(code_200_page_response)
+code_300_page_response = requests.get("https://httpbin.org/status/300")
+log.info(code_300_page_response)
+code_400_page_response = requests.get("https://httpbin.org/status/400")
+log.info(code_400_page_response)
+code_500_page_response = requests.get("https://httpbin.org/status/500")
+log.info(code_500_page_response)
---
+++ Test_for_raidix.py
@@ -0,0 +1,67 @@
+# Тесты для "httpbin.org".
+# Эндпоинты:
+# "https://httpbin.org/headers",
+# "https://httpbin.org/status/200",
+# "https://httpbin.org/status/300",
+# "https://httpbin.org/status/400",
+# "https://httpbin.org/status/500".
+#
+# Отчёт о тестировании генерируется в файл report.html посредством pytest-html
+# с аргументами конфигурации "--html=report.html --self-contained-html".
+
+import SetUp_for_tests as St
+
+
+# Класс с тестами для эндпоинта "https://httpbin.org/headers".
+class TestHttpbinHeadersPage:
+
+    # Тест на соответствие статус кода в ответе коду 200.
+    def test_headers_check_status_code_equals_200(self):
+        assert St.headers_page_response.status_code == 200, "Код ответа не равен 200."
+
+    # Тест на соответствие значения заголовка Content-Type формату JSON.
+    def test_headers_check_content_type_equals_json(self):
+        assert St.headers_page_response.headers["Content-Type"] \
+               == "application/json", "Значение заголовка Content-Type не идентифицирует тело ответа как формат JSON."
+
+    # Тест на соответствие значения заголовка Server требуемой версии: gunicorn/19.9.0.
+    def test_headers_check_server_equals_required(self):
+        assert St.headers_page_response.headers["Server"] == "gunicorn/19.9.0", "Тип сервера не идентифицируется как " \
+                                                                                "требеумый: gunicorn/19.9.0"
+
+    # Тест на соответствие значения Host в теле headers значению "httpbin.org".
+    def test_headers_check_host_equals_httpbin_org(self):
+        assert St.headers_page_response_body["headers"]["Host"] \
+               == "httpbin.org", "Значение Host не соответствует 'httpbin.org'"
+
+    # Тест на соответствие значения Accept в теле headers значению "*/*".
+    def test_headers_check_accept_equals_required(self):
+        assert St.headers_page_response_body["headers"]["Accept"] == "*/*"
+
+    # Тест на соответствие значения Accept-Encoding в теле headers значению "gzip, deflate".
+    def test_headers_check_accept_encoding_equals_gzip_deflate(self):
+        assert St.headers_page_response_body["headers"]["Accept-Encoding"] == "gzip, deflate"
+
+    # Тест на соответствие значения User-Agent в теле headers значению "python-requests/2.27.1".
+    def test_headers_check_user_agent_equals_python_requests(self):
+        assert St.headers_page_response_body["headers"]["User-Agent"] == "python-requests/2.27.1"
+
+
+# Класс с тестами для эндпоинтов "https://httpbin.org/status/статус_код".
+class TestHttpbinStatusCodePages:
+
+    # Тест на соответствие статус кода в ответе коду 200.
+    def test_code_200_check_equals_200(self):
+        assert St.code_200_page_response.status_code == 200, "Код ответа не равен 200."
+
+    # Тест на соответствие статус кода в ответе коду 300.
+    def test_code_300_check_equals_300(self):
+        assert St.code_300_page_response.status_code == 300, "Код ответа не равен 300."
+
+    # Тест на соответствие статус кода в ответе коду 400.
+    def test_code_400_check_equals_400(self):
+        assert St.code_400_page_response.status_code == 400, "Код ответа не равен 400."
+
+    # Тест на соответствие статус кода в ответе коду 500.
+    def test_code_200_check_equals_500(self):
+        assert St.code_500_page_response.status_code == 500, "Код ответа не равен 500."
